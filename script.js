eb3877a80337cc7872b876ae7058c21f6ca8076e
Adarsh4775 <137145969+Adarsh4775@users.noreply.github.com>
2023-11-14 18:01:29 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,6 +1,15 @@
 function makeid(l) {
   // write your code here
-}
+	let charNumLoop = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+	let randomStr="";
+
+	for(i=0; i<l; i++)
+		{
+			let randomIndex = Math.floor(Math.random() * charNumLoop.length);
+			randomStr += charNumLoop[randomIndex];
+		}
+	return randomStr;
+} 
 
 // Do not change the code below.
 const l = prompt("Enter a number.");
